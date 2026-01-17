1c04890a766538ac074f3bab5d52b41044e224ef
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-01-17 19:38:53 +0530

    toggle theme logic

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ const observer = new IntersectionObserver(entries => {
 }, {threshold: 0.15} );
 
 sections.forEach(section => observer.observe(section));
+
+
+// ------ Dark light mode toggle ------- //
+const toggle = document.getElementById('themeToggle');
+const body = document.body;
+
+// check saved theme
+const savedTheme = localStorage.getItem('portfolio-theme');
+if (savedTheme === 'light') {
+    body.classList.add('light-mode');
+    toggle.textContent = '☀️';
+}
+
+// toggle theme on click
+toggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+
+    const isLight = body.classList.contains('light-mode');
+    toggle.textContent = isLight ? '☀️' : '🌙';
+
+    localStorage.setItem('portfolio-theme', isLight ? 'light' : 'dark');
+});
